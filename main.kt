68e3c0c488aec71d0e3f4e1d6df13da5eba00961
Cubidev3 <100206395+Cubidev3@users.noreply.github.com>
2023-07-19 07:03:58 -0300

    remake

--- main.kt
+++ main.kt
@@ -1,84 +1,69 @@
-package penisdetbot
-
 import dev.kord.core.Kord
+import dev.kord.core.behavior.reply
+import dev.kord.core.entity.Message
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
-import kotlinx.coroutines.flow.*
 
-suspend fun main () {
-    val wordsToCheck: List<String> = listOf("buceta", "rosca", "rola", "penis", "pinto", "hentai", "yamete")
-    val client = Kord("<Your Token>")
+const val token = "<Insert your token here>"
 
-    client.events
-        .filterIsInstance<MessageCreateEvent>()
-        .map { it.message }
-        .filter { it.author?.isBot == false }
-        .onEach {
-            for (wordToCheck in wordsToCheck) {
-                val hidden = getHiddenWordsInPhraseOrNothing(wordToCheck, it.content)
-                if (hidden.isNotEmpty()) {
-                    if (it.author != null) {
-                        it.channel.createMessage(hiddenMessageDiscoveredMessage(wordToCheck, hidden, it.author!!.id.value))
-                    }
-                }
-            }
-        }
-        .launchIn(client)
+val wordsToSearchFor = listOf<String>("penis", "rosca", "furico", "sexo", "buceta", "giromba", "gabryeltwink", "jocagay", "twink", "daddy", "yametekudasai", "suruba", "pinto", "xvideos")
 
-    client.login {
+suspend fun main() {
+    val bot = Kord(token)
+
+    bot.on<MessageCreateEvent>
+    {
+        if (message.author?.isBot!!) return@on
+        exposeHiddenWords(message)
+    }
+
+    bot.login {
+        // we need to specify this to receive the content of messages
         @OptIn(PrivilegedIntent::class)
         intents += Intent.MessageContent
     }
 }
 
-fun getHiddenWordsInPhraseOrNothing(hiddenWord: String, phrase: String): List<List<String>> {
-    val words = phrase.split(' ')
-    val hiddenWordsIndexes: MutableList<Int> = mutableListOf()
-    val hiddenWordsList: MutableList<List<String>> = mutableListOf()
+suspend fun exposeHiddenWords(message: Message)
+{
+    wordsToSearchFor.forEach { word ->
+        val hiddenWordIndices = hiddenWordIndices(message.content, word)
+        if (hiddenWordIndices.count() != word.count()) return@forEach
 
-    for (word in words.indices) {
-        if (wordStartWith(words[word], hiddenWord[hiddenWordsIndexes.size])) hiddenWordsIndexes.add(word)
-        if (hiddenWordsIndexes.size == hiddenWord.length) {
-            var hiddenWords = mutableListOf<String>()
-            for (i in hiddenWordsIndexes.indices) {
-                var wordToAdd = "${words[hiddenWordsIndexes[i]]}"
-                if ((i == 0 && hiddenWordsIndexes[i] != 0) || (i != 0 && (hiddenWordsIndexes[i] - hiddenWordsIndexes[i - 1]) > 1)) {
-                    wordToAdd = "..$wordToAdd"
-                }
-
-                if ((i == hiddenWordsIndexes.lastIndex && hiddenWordsIndexes[i] != words.lastIndex) || (i != hiddenWordsIndexes.lastIndex && (hiddenWordsIndexes[i + 1] - hiddenWordsIndexes[i]) > 1)) {
-                    wordToAdd = "$wordToAdd.."
-                }
-                hiddenWords.add(wordToAdd)
-            }
-
-            hiddenWordsList.add(hiddenWords)
-            hiddenWordsIndexes.clear()
-        }
+        message.reply { content = hiddenWordWasFoundMessage(message.content, hiddenWordIndices)}
     }
-
-    return hiddenWordsList
 }
 
-fun wordStartWith(word: String, specialLetter: Char): Boolean {
-    val firstLetter = word.firstOrNull() ?: return false
-    if (firstLetter.lowercaseChar() != specialLetter) return false
-    return true
+fun hiddenWordIndices(message: String, hiddenWord: String) : List<Int>
+{
+    val indicesFound = mutableListOf<Int>()
+
+    message.split(" ").map { it.first() }.forEachIndexed { index, initial ->
+        val matchesHiddenChar = initial.lowercaseChar() == hiddenWord.getOrNull(indicesFound.count())?.lowercaseChar()
+        if (matchesHiddenChar) indicesFound.add(index)
+    }
+
+    return indicesFound
 }
 
-fun hiddenMessageDiscoveredMessage(hiddenWord: String, words: List<List<String>>, messageOwner: ULong): String {
-    var message = "<@$messageOwner> \n"
-    for (wordlist in words) {
-        message += "// "
-        for (word in wordlist) {
-            message += ("${word.slice(0 until word.indexOfFirst { it != '.' } )}**${word.first{it != '.'}.uppercase()}**${word.slice((word.indexOfFirst { it != '.' } + 1) until word.length)} ")
-        }
-        message += "\n"
-    }
-    message += "\n"
-    val sex = if (hiddenWord.last() == 'a') 'a' else 'o'
-    message += "**${hiddenWord.uppercase()}** Secret$sex Detectad$sex! \n"
+fun hiddenWordWasFoundMessage(message: String, wordIndices: List<Int>) : String
+{
+    val words = message.split(" ")
 
-    return message
+    val messageHead = wordIndices.zipWithNext { index, nextIndex ->
+        when (nextIndex) {
+            index + 1 -> "${words[index].replaceFirstChar { "**${it.uppercaseChar()}**" }} "
+            else -> "${words[index].replaceFirstChar { "**${it.uppercaseChar()}**" }}.. .."
+        }
+    }.joinToString("")
+
+    // As a side effect of using zipWithNext, the last element is removed (because there is no next element to it)
+    val last = words[wordIndices.last()].replaceFirstChar { "**${it.uppercaseChar()}**" }
+
+    val wordFoundMessage = wordIndices.joinToString("") { index -> words[index].first().uppercase() }
+
+    return "// $messageHead $last + \n" +
+            "Um **$wordFoundMessage** Secreto foi Encontrado!"
 }
